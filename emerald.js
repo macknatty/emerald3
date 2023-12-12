cc21c4a19fc1b166c17ef5bb3d2d08224d149589
macknatty <75257015+macknatty@users.noreply.github.com>
2023-12-12 12:38:23 +0300

    Add files via upload

---
+++ emerald.js
@@ -0,0 +1,197 @@
+/*-----------------------------------------adam------------------------------------------------------- */
+function chek() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="1920,0" --user-data-dir="C:/tmp/Profiles/1" -kiosk -fullscreen -incognito  "http://localhost:8000/#/?cdm=4&url=https://api.board.bluebez.com/keno"');
+}
+
+function ches() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="1280,0" --user-data-dir="C:/tmp/Profiles/2" -kiosk -fullscreen -incognito  "http://localhost:9000/#/?cdm=4&url=https://api.board.bluebez.com/spin&dm=1"')
+}
+
+function chec() {
+  navigator.clipboard.writeText
+      ('https://retail.board.bluebez.com');
+}
+
+/*-----------------------------------------elias------------------------------------------------------- */
+function chec() {
+  navigator.clipboard.writeText
+      ('https://retail.board.bluebez.com');
+}
+function chec() {
+  navigator.clipboard.writeText
+      ('https://retail.board.bluebez.com');           /* elias */
+}     
+function chec() {
+  navigator.clipboard.writeText
+      ('https://retail.board.bluebez.com');
+}
+/*------------------------------------------hab------------------------------------------------------ */
+
+function bk() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="0,0" --user-data-dir="C:/tmp/Profiles/1" -kiosk -fullscreen -incognito  "http://localhost:8000/#/?cdm=4&url=https://api.equal.bluebez.com/keno"');
+}
+function bs() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="0,0" --user-data-dir="C:/tmp/Profiles/2" -kiosk -fullscreen -incognito  "http://localhost:9000/#/?cdm=4&url=https://api.equal.bluebez.com/spin&dm=2" ');           
+}     
+function bc() {
+  navigator.clipboard.writeText
+      ('https://retail.equal.bluebez.com');}
+
+function ck() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="0,0" --user-data-dir="C:/tmp/Profiles/1" -kiosk -fullscreen -incognito  "http://localhost:8000/#/?cdm=4&url=https://api-crown.emerald-games.com/keno"');
+}
+function cs() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="1280,0" --user-data-dir="C:/tmp/Profiles/2" -kiosk -fullscreen -incognito  "http://localhost:9000/#/?cdm=4&url=https://api-crown.emerald-games.com/spin&dm=1"');           
+}     
+function cc() {
+  navigator.clipboard.writeText
+      ('https://retail-crown-bet.emerald-games.com');}
+
+
+   function lek() {
+        navigator.clipboard.writeText
+            ('start "" chrome.exe --new-window --window-position="0,0" --user-data-dir="C:/tmp/Profiles/1" -kiosk -fullscreen -incognito  "http://localhost:8000/#/?cdm=4&url=https://api.hero.bluebez.com/keno"');
+      }
+   function les() {
+        navigator.clipboard.writeText
+            ('');          
+      }     
+   function lec() {
+        navigator.clipboard.writeText
+            (' https://retail.hero.bluebez.com');}
+
+            
+function iconk() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="0,0" --user-data-dir="C:/tmp/Profiles/1" -kiosk -fullscreen -incognito  "http://localhost:8000/#/?cdm=4&url=https://api.fav.bluebez.com/keno"');
+}
+function icons() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="0,0" --user-data-dir="C:/tmp/Profiles/2" -kiosk -fullscreen -incognito  "http://localhost:9000/#/?cdm=4&url=https://api.fav.bluebez.com/spin&dm=1" ');           
+}
+      function iconc() {
+  navigator.clipboard.writeText
+      ('https://retail.fav.bluebez.com');
+
+
+
+}
+/*---------------------------------------muna--------------------------------------------------------- */
+
+function holak() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="0,0" --user-data-dir="C:/tmp/Profiles/1" -kiosk -fullscreen -incognito  "http://localhost:8000/#/?cdm=4&url=https://api-honda.emerald-games.com/keno"');
+}
+function holas() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="0,0" --user-data-dir="C:/tmp/Profiles/2" -kiosk -fullscreen -incognito  "http://localhost:9000/#/?cdm=4&url=https://api-honda.emerald-games.com/spin&dm=1"');          
+}     
+function holac() {
+  navigator.clipboard.writeText
+      ('https://retail-honda.emerald-games.com');
+
+}
+
+
+function artk() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="0,0" --user-data-dir="C:/tmp/Profiles/1" -kiosk -fullscreen -incognito  "http://localhost:8000/#/?cdm=4&url=https://api-art.emerald-games.com/keno"');
+}
+function arts() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="0,0" --user-data-dir="C:/tmp/Profiles/2" -kiosk -fullscreen -incognito  "http://localhost:9000/#/?cdm=4&url=https://api-art.emerald-games.com/spin&dm=1"');      
+  navigator.clipboard.writeText
+      ('https://retail-art.emerald-games.com');
+}
+
+
+function royalk() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="0,0" --user-data-dir="C:/tmp/Profiles/1" -kiosk -fullscreen -incognito  "http://localhost:8000/#/?cdm=4&url=https://api-royal.emerald-games.com/keno"');
+}
+function royals() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="0,0" --user-data-dir="C:/tmp/Profiles/2" -kiosk -fullscreen -incognito  "http://localhost:9000/#/?cdm=4&url=https://api-royal.emerald-games.com/spin&dm=1"');           /*muna */
+}     
+function royalc() {
+  navigator.clipboard.writeText
+      ('https://retail-royal.emerald-games.com');
+}
+
+/*----------------------------------naty-------------------------------------------------------------- */
+
+
+function nicek() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="1920,0" --user-data-dir="C:/tmp/Profiles/1" -kiosk -fullscreen -incognito  "http://localhost:8000/#/?cdm=4&url=https://api.cool.bluebez.com/keno"');
+}
+function nices() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="1920,0" --user-data-dir="C:/tmp/Profiles/2" -kiosk -fullscreen -incognito  "http://localhost:9000/#/?cdm=4&url=https://api.cool.bluebez.com/spin&dm=1"');
+}
+function nicec() {
+  navigator.clipboard.writeText
+      ('https://retail.cool.bluebez.com');
+}
+function premierk() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="1920,0" --user-data-dir="C:/tmp/Profiles/1" -kiosk -fullscreen -incognito  "http://localhost:8000/#/?cdm=4&url=https://api.uk.bluebez.com/keno"');
+}
+function premiers() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="1920,0" --user-data-dir="C:/tmp/Profiles/2" -kiosk -fullscreen -incognito  "http://localhost:9000/#/?cdm=4&url=https://api.uk.bluebez.com/spin&dm=1"');
+}
+function premierc() {
+  navigator.clipboard.writeText
+      ('https://retail.uk.bluebez.com');
+}
+
+/*----------------------------------------yeab------------------------------------------------------*/
+function coffeek() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="0,0" --user-data-dir="C:/tmp/Profiles/1" -kiosk -fullscreen -incognito  "http://localhost:8000/#/?cdm=4&url=https://api-coffee.emerald-games.com/keno"');
+}
+function coffees() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="0,0" --user-data-dir="C:/tmp/Profiles/2" -kiosk -fullscreen -incognito  "http://localhost:9000/#/?cdm=4&url=https://api-coffee.emerald-games.com/spin&dm=2"');          
+}     
+function coffeec() {
+  navigator.clipboard.writeText
+      ('https://retail-coffee.emerald-games.com');
+}
+
+
+
+function empirek() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="0,0" --user-data-dir="C:/tmp/Profiles/1" -kiosk -fullscreen -incognito  "http://localhost:8000/#/?cdm=4&url=https://api-kingdom.emerald-games.com/keno"');
+}
+function empires() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="0,0" --user-data-dir="C:/tmp/Profiles/2" -kiosk -fullscreen -incognito  "http://localhost:9000/#/?cdm=4&url=https://api-kingdom.emerald-games.com/spin&dm=2"');          
+}     
+function empirec() {
+  navigator.clipboard.writeText
+      ('https://retail-kingdom.emerald-games.com');
+}
+
+
+
+function enjoyk() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="0,0" --user-data-dir="C:/tmp/Profiles/1" -kiosk -fullscreen -incognito  "http://localhost:8000/#/?cdm=4&url=https://api-hat.emerald-games.com/keno"');
+}
+function enjoys() {
+  navigator.clipboard.writeText
+      ('start "" chrome.exe --new-window --window-position="0,0" --user-data-dir="C:/tmp/Profiles/2" -kiosk -fullscreen -incognito  "http://localhost:9000/#/?cdm=4&url=https://api-hat.emerald-games.com/spin&dm=1"');           
+}     
+function enjoyc() {
+  navigator.clipboard.writeText
+      ('https://retail-hat.emerald-games.com');
+}
